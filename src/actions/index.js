715b5af940d2646cf32862450adef61a7f5ba063
Alvin Allen <alaj6625@gmail.com>
2019-02-14 10:07:17 -0600

    added actions

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+export const voteAngular = () => {
+   return {
+     type: 'VOTE_ANGULAR'
+   }
+ }
+ export const voteReact = () => {
+   return {
+     type: 'VOTE_REACT'
+   }
+ }
+ export const voteVuejs = () => {
+   return {
+     type: 'VOTE_VUEJS'
+   }
+ }
+ 
